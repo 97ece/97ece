588557a173f7d42a5c0ab96665a7deecdd076aee
97ece <30343837+97ece@users.noreply.github.com>
2017-08-12 16:48:42 +0530

    Update and rename popp.c to ooip.c

--- ooip.c
+++ ooip.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int b;
+int d;
 printf("/n enter a number");
-scanf("%d",&b);
-if(b%2==0)
+scanf("%d",&d);
+if(d%2==0)
 {
 printf("/n num is even");
 }
@@ -12,5 +12,5 @@ else
 {
 printf("/n num is odd");
 }
-return 0;
+return (0);
 }
